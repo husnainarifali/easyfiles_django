73bd5e0db04979eb5f10513965f66afb13bbeca6
Zohaib Yousaf <87264291+zohaib3249@users.noreply.github.com>
2023-02-04 15:48:51 +0500

    Update settings.py

--- SMS/settings.py
+++ SMS/settings.py
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'easyfilesforu@gmail.com'
-EMAIL_HOST_PASSWORD = 'xcucznwvzpznmltg'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
